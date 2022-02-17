770e1b570082d14ae25756aae6dd19287b36016b
Kathleen Ly <lykathleen@outlook.com>
2022-02-17 06:08:32 +0000

    Completed exercise

--- lib/garage.rb
+++ lib/garage.rb
@@ -6,4 +6,33 @@ class Garage
       {registration_plate: 'SE60 TSW', make: 'honda'}
     ]
   end
+
+  def add(car)
+    @cars << car
+  end
+
+  def find(rego)
+    @cars.find do |car| 
+      car[:registration_plate] == rego
+    end
+  end
+
+  def remove(rego)
+    @cars.delete_if do |car|
+      car[:registration_plate] == rego
+    end
+    
+  end
+
+  def all_of_make(make)
+    @cars.select do |car| 
+      car[:make] == make
+    end
+    
+  end
+
+  def cars
+    @cars
+  end
+
 end
